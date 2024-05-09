c507bcb2613c3bcfc5b4272d123d100bb21843e6
sebasXDXD <sebastianurbina.idetp@gmail.com>
2024-05-09 12:30:10 -0500

    se agrega nueva tabla

--- db/url_short_db.sql
+++ db/url_short_db.sql
@@ -19,8 +19,52 @@ CREATE TABLE users (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- Crear el gatillo para llamar a la función antes de una actualización en users
+-- Crear el trigger para llamar a la función antes de una actualización en users
 CREATE TRIGGER users_update_updated_at
 BEFORE UPDATE ON users
 FOR EACH ROW
 EXECUTE FUNCTION update_updated_at();
+
+-- Crear la tabla links
+CREATE TABLE links (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    redirect_to TEXT,
+    user_created_id INT REFERENCES users(id),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    is_deleted BOOLEAN DEFAULT FALSE,
+    deleted_at TIMESTAMP
+);
+
+-- Crear un trigger para actualizar updated_at cuando se realicen cambios en la fila
+CREATE OR REPLACE FUNCTION update_links_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER links_updated_at_trigger
+BEFORE UPDATE ON links
+FOR EACH ROW
+EXECUTE FUNCTION update_links_updated_at();
+
+-- Crear un trigger para establecer deleted_at cuando is_deleted se establece como true
+CREATE OR REPLACE FUNCTION update_links_deleted_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.is_deleted = TRUE THEN
+        NEW.deleted_at = CURRENT_TIMESTAMP;
+    ELSE
+        NEW.deleted_at = NULL;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER links_deleted_at_trigger
+BEFORE UPDATE ON links
+FOR EACH ROW
+EXECUTE FUNCTION update_links_deleted_at();
