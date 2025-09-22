4989eabc2bf62aa5e8b0c3ae012b23d9e28ab7bc
sebasXDXD <sebastianurbina.idetp@gmail.com>
2025-09-22 14:39:39 -0500

    se mejora el db.go y las variables de entorno

--- db/db.go
+++ db/db.go
@@ -9,19 +9,25 @@ import (
 )
 
 func ConnectDb() (*sql.DB, error) {
-	// Obtener las variables de entorno
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	sslmode := os.Getenv("DB_SSLMODE")
+	var dataSourceName string
 
-	// Crear el string de conexión
-	dataSourceName := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		host, port, user, password, dbname, sslmode,
-	)
+	// Si existe DATABASE_URL, la usamos (Render lo da así)
+	if os.Getenv("DATABASE_URL") != "" {
+		dataSourceName = os.Getenv("DATABASE_URL")
+	} else {
+		// Caso local: usamos las variables separadas del .env
+		host := os.Getenv("DB_HOST")
+		port := os.Getenv("DB_PORT")
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		dbname := os.Getenv("DB_NAME")
+		sslmode := os.Getenv("DB_SSLMODE")
+
+		dataSourceName = fmt.Sprintf(
+			"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+			host, port, user, password, dbname, sslmode,
+		)
+	}
 
 	// Conectar
 	db, err := sql.Open("postgres", dataSourceName)
